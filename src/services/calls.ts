5193937c26e8d4cea8b9b04f9b649209190b7068
Fernando <fernandojfenley@gmail.com>
2021-11-02 20:25:03 -0300

    Comentários nos arquivos api.ts e calls.ts

--- src/services/calls.ts
+++ src/services/calls.ts
@@ -3,31 +3,35 @@ import { Buffer } from 'buffer';
 
 import MessageBuffer from '../services/buffer/MessageBuffer';
 
-//PropertiesProps
+/*
+  Estrutura TypeScript utilizada para tipar os objetos das propriedades na aplicação.
+  Desta forma, os dados armazenados em memória referente às propriedades terão
+  os atributos e tipos especificados na estrutura abaixo:
+*/
 export type PropertiesProps = {
-  property_id: string;
+  property_id: string; // identificador único
   location: {
     address: {
-      line: string;
-      state: string;
+      line: string; // enderçeo completo
+      state: string; // estado
     };
     neighborhoods: {
-      name: string;
+      name: string; // bairro
     }[];
   };
-  list_price_max: number;
+  list_price_max: number; // preço máximo para aluguel
   lot_size: {
-    size: number;
-    units: string;
+    size: number; // tamanho do lote
+    units: string; // quantidade de unidades no lote
   };
   description: {
-    beds_max: number;
-    baths_max: number;
-    garage_max: number;
+    beds_max: number; // quantidade de quartos na propriedade
+    baths_max: number; // quantidades de banheiros na propriedade
+    garage_max: number; // quantidade de baragens na propriedade
     sqft_max: number;
   };
   photos: {
-    href: string;
+    href: string; // URL com a imagem da propriedade
   }[];
   tags?: {
     text: string[];
@@ -38,14 +42,26 @@ type ResponseProps = {
   properties: PropertiesProps[];
 };
 
+/*
+  Estrutura TypeScript utilizada para tipar os parâmetros dos filtros de propriedade.
+  Desta forma, todas as vezes que a aplicação necessitar enviar os parâmetros dos filtros
+  ao servidor poderá consultar os objetos oriundos dessa estrutura.
+*/
 export type FilteringParamsProps = {
-  type?: string | boolean;
-  beds?: string;
-  price: Array<Number>;
-  baths?: string;
-  garages?: string;
+  type?: string | boolean; // tipo de propriedade filtrada pelo usuário: para venda = for_sale(false) e para aluguel = for_rent(true)
+  price: Array<Number>; // array que contem o preço mímimo e máximo desejado para alugel ou compra
+  beds?: string; // quantidade mínima de quartos desejado na propriedade
+  baths?: string; // quantidade mínima de quartos desejado na propriedade
+  garages?: string; // quantidade mínima de garagens desejado na propriedade
 };
 
+/*
+  Função responsável pelo envio de mensagens ao servidor via sockets e recebimento da resposta refernte
+  a dados das propriedades. Implementamos uma classe chamada 'MessageBuffer', cujo conceito pode ser encontrado no relatório do projeto.
+  Basicamente, o buffer permite armazenar os 'pedaços' das mensagens recebidas do servidor e armazená-los em um buffer até encontrar
+  um delimitador. No caso do nosso projeto, o delimitador padrão utilizado por todas as mensagens
+  é "\n". Quando o buffer identificar o delimitador, é possível fazer o parse da mensagem completa.
+*/
 function handleOnReceiveProperties(msg: string): PropertiesProps[] {
   let properties: PropertiesProps[] = [];
   client.write(msg);
@@ -72,12 +88,30 @@ function handleOnReceiveProperties(msg: string): PropertiesProps[] {
   return properties;
 }
 
+/*
+  Função responsável por criar o formato da mensagem que solicita ao servidor
+  os dados das propriedades armazenadas no banco de dados.
+
+  Formato da mensagem: properties_list\n
+*/
 export async function getHousesCall(): Promise<PropertiesProps[]> {
   let properties = handleOnReceiveProperties('properties_list\n');
 
   return properties;
 }
 
+/*
+  Função responsável por criar o formato da mensagem para filtragem de proprieades do tipo
+  'for_sale', ou seja, que estão à venda
+  Formato da mensagem: for_sale;PRICE_MIN;PRICE_MAX;BEDS;BATHS;GARAGES\n
+
+  Em que:
+  PRICE_MIN=preço mínimo solicitado pelo usuário (inteiro)
+  PRICE_MAX=preço máximo filtr pelo usuário (inteiro)
+  BEDS=quantidade mínima de quartos na propriedade (inteiro)
+  BATHS=quantidade mínima de banheiros na proprieade (inteiro)
+  GARAGES=quantidade mínima de garagens na proprieade (inteiro)
+*/
 export async function getForSaleHousesCall(
   offset: number,
   filteringParams: FilteringParamsProps,
@@ -108,6 +142,17 @@ export async function getForSaleHousesCall(
   return properties;
 }
 
+/*
+  Função responsável por criar o formato da mensagem para filtragem de proprieades do tipo
+  'for_rent', ou seja, que estão disponíveis para alugar
+  Formato da mensagem: for_;PRICE_MIN;PRICE_MAX;BEDS;BATHS;GARAGES\n
+  Em que;
+  PRICE_MIN=preço mínimo solicitado pelo usuário (inteiro)
+  PRICE_MAX=preço máximo filtr pelo usuário (inteiro)
+  BEDS=quantidade mínima de quartos na propriedade (inteiro)
+  BATHS=quantidade mínima de banheiros na proprieade (inteiro)
+  GARAGES=quantidade mínima de garagens na proprieade (inteiro)
+*/
 export async function getForRentHousesCall(
   offset: number,
   filteringParams: FilteringParamsProps,
@@ -117,7 +162,7 @@ export async function getForRentHousesCall(
 
     filteringParams.price[1] = 20000;
   }
-  //console.log(filteringParams);
+
   const mensagem: string =
     filteringParams.type +
     ';' +
@@ -134,11 +179,19 @@ export async function getForRentHousesCall(
 
   let properties = handleOnReceiveProperties(mensagem);
 
-  //console.log(mensagem);
-
   return properties;
 }
 
+/*
+  Função responsável por criar o formato da mensagem que solicita ao servidor
+  informações adicionais a respeito de uma propriedade específica.
+  Formato da mensagem: property_id=PROPERTY_ID\n
+
+  Os dados da mensagem de retorno do servidor é tratado por meio do MessageBuffer explicado anteriormente.
+
+  Em que:
+  PROPERTY_ID=identificador único de uma propeirdade do banco de dados
+*/
 var property;
 
 export function getHouseDetailsCall(property_id: string): PropertiesProps {
@@ -163,6 +216,14 @@ export function getHouseDetailsCall(property_id: string): PropertiesProps {
   return property;
 }
 
+/*
+  Função responsável por criar o formato da mensagem para solicitar ao servidor
+  o envio das localização das propriedades armazenadas no banco de dados.
+
+  Para isso, a função prepara a seguinte mensagem: locations suggestion\n
+
+  Os dados da mensagem de retorno do servidor é tratado por meio do MessageBuffer explicado anteriormente.
+*/
 export function getLocationsListCall() {
   var locationsList: any = [];
 
